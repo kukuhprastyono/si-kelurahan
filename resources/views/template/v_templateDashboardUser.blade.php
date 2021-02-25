74e1483539a746af9520c00456fe0b77ef4e98e3
Kukuh Prastyono <prastyonokukuh@gmail.com>
2021-02-25 17:10:50 +0700

    http fix

--- resources/views/template/v_templateDashboardUser.blade.php
+++ resources/views/template/v_templateDashboardUser.blade.php
@@ -16,7 +16,7 @@
     <!-- Custom fonts for this template-->
     <link href="{{url('sb-admin-2')}}/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        href="http://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
